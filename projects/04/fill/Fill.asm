45f88bbe52ce294a5c88873ac11fd607e4feff61
Michael Thelander <mjthelander@gmail.com>
2020-01-31 21:41:12 -0800

    Project 4 done

--- projects/04/fill/Fill.asm
+++ projects/04/fill/Fill.asm
@@ -6,9 +6,61 @@
 // Runs an infinite loop that listens to the keyboard input.
 // When a key is pressed (any key), the program blackens the screen,
 // i.e. writes "black" in every pixel;
-// the screen should remain fully black as long as the key is pressed. 
+// the screen should remain fully black as long as the key is pressed.
 // When no key is pressed, the program clears the screen, i.e. writes
 // "white" in every pixel;
 // the screen should remain fully clear as long as no key is pressed.
 
-// Put your code here.
+// while true:
+//         i = &screen
+//         for pixelgroup in (0..256):
+//             for col in (0..32):
+//                 if KEY:
+//                     SCREEN[i++] = 1111111111111111
+//                 else:
+//                     SCREEN[i++] = 0
+
+(INFINITELOOP)
+    @8192
+    D=A            // D = 256*32
+    @PIXELGROUP
+    M=D            // pixelgroup = 256*32
+    @SCREEN
+    D=A            // D = &screen
+
+    @PIXELGROUPPOINTER
+    M=D            // pixelgrouppointer = &screen
+
+    (ROWLOOP)
+        @KBD
+        D=M        // D = keypressed
+
+        @BLACK
+        D;JGT      // jump to (black) if keypressed > 0
+
+        (WHITE)
+            @PIXELGROUPPOINTER
+            D=M
+            A=D
+            M=0     // *pixelgrouppointer = 0
+            @ENDBLACK
+            0;JMP
+        (BLACK)
+            @PIXELGROUPPOINTER
+            D=M
+            A=D
+            D=0
+            D=!D
+            M=D     // *pixelgrouppointer = 1111111111111111
+        (ENDBLACK)
+
+        @PIXELGROUPPOINTER
+        M=M+1
+
+        @PIXELGROUP
+        M=M-1       // pixelgroup = pixelgroup - 1
+        D=M
+        @ROWLOOP
+        D;JGT       // jump to (rowloop) if pixelgroup > 0
+    @INFINITELOOP
+    0;JMP
--- projects/04/mult/mult.asm
+++ projects/04/mult/mult.asm
@@ -6,4 +6,27 @@
 // Multiplies R0 and R1 and stores the result in R2.
 // (R0, R1, R2 refer to RAM[0], RAM[1], and RAM[2], respectively.)
 
-// Put your code here.
+@R0
+D=M
+@counter
+M=D   // counter = R0
+
+@R2
+M=0 // R2 = 0
+
+@END
+D;JEQ // goto end if R0 == 0
+
+(MULT)
+    @R1
+    D=M
+    @R2
+    M=M+D  // R2 = R2+R1
+
+    @counter
+    M=M-1  // counter = counter - 1
+    D=M
+
+    @MULT
+    D;JGT
+(END)
